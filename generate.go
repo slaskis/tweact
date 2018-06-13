ff9b00bc662b9ad2fc8994ec58ca46216abc08ed
Robert Sköld <robert@publicclass.se>
2018-06-13 09:01:15 +0200

    Added retool for easier build

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
-//go:generate protoc -I pkg:rpc:vendor --lint_out=pkg --gogofast_out=pkg --twirp_out=pkg rpc/todos/v1/service.proto
+//go:generate retool do protoc -I pkg:rpc:vendor --lint_out=pkg --go_out=pkg --twirp_out=pkg rpc/todos/v1/service.proto
 
 package main
