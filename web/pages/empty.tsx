89853651e500d1c5b593cdbe0d415b549897f0d1
Robert Sköld <robert@publicclass.se>
2018-07-20 22:46:59 +0200

    Added a page without any twirp to test size difference

---
+++ web/pages/empty.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/nav";
+import Head from "../components/head";
+
+const App = () => {
+  return (
+    <div>
+      <Head title="Home" />
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
