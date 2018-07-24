901a7928c5fa06797ce8de151a6445c9d4b30e92
Robert Sköld <robert@publicclass.se>
2018-07-24 19:03:32 +0200

    Renamed to make a little more sense

--- web/rpc/demo/DemoService.ts
+++ web/rpc/demo/DemoService.ts
@@ -14,35 +14,38 @@ import { withTwirp, TwirpService } from "@department/twirp-component";
  * EchoRequest only passes a message
  */
 export interface EchoRequest {
-  /**
-   * message to echo
-   */
+	
+/**
+ * message to echo
+ */
   message: string;
 }
 /**
  * EchoResponse only contains a message
  */
 export interface EchoResponse {
-  /**
-   * message that was in the request
-   */
+	
+/**
+ * message that was in the request
+ */
   message: string;
 }
 /**
  * TestEnum has a comment
  */
 export enum Test {
+  
   UNKNOWN = 0,
-
-  /**
-   * So does this value
-   */
+  
+/**
+ * So does this value
+ */
   HELLO = 1,
-
-  /**
-   * and trailing
-   */
-  THERE = 2
+  
+/**
+ * and trailing
+ */
+  THERE = 2,
 }
 
 /**
